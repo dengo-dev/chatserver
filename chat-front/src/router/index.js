55d8942b9894e611c2b04e5a38259317c50c5342
dengo23-dev <dengo23.dev@gmail.com>
2025-04-05 14:17:26 +0900

    front

---
+++ chat-front/src/router/index.js
@@ -0,0 +1,30 @@
+import {createRouter, createWebHistory} from "vue-router";
+import MemberCreate from "@/views/MemberCreate.vue"
+import LoginPage from "@/views/LoginPage.vue"
+import MemberList from "@/views/MemberList.vue"
+
+const routes = [
+    {
+        path: '/member/create',
+        name: 'MemberCreate',
+        component: MemberCreate
+    },
+    {
+        path: '/login',
+        name: 'LoginPage',
+        component: LoginPage
+    },
+    {
+        path: '/member/list',
+        name: 'MemberList',
+        component: MemberList
+    }
+
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+export default router;
